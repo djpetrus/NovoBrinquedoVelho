4ed5b7f1e4711697705fbf9d3f53def751df45bc
Petrus Ribeiro Lima da Costa <djpetrus@gmail.com>
2022-01-02 15:32:39 -0300

    Initial Commit

---
+++ NovoBrinquedoVelho/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NovoBrinquedoVelho
+//
+//  Created by Petrus Ribeiro Lima da Costa on 02/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
